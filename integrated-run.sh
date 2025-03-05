cf5b2679f424d71784db3fdc52d3c15e5aae10ad
Claire Vlases <cvlases25@cmc.edu>
2025-03-04 20:30:08 -0800

    updated comments

--- integrated-run.sh
+++ integrated-run.sh
@@ -15,6 +15,9 @@ sim2Dir = "sim_2"
 sim3Dir = "sim_3"
 sim4Dir = "sim_4"
 
+# Runs 4 simulations of GROMACS
+## Calls the run_gmx script
+## Outputs into the simulation folders
 bsub -nnodes 1 -W 60 -G cancer -q pbatch ${cDir}/run_gmx.sh 1 ${cDir}/${sim1Dir} ${cDir}/${sim2Dir} ${cDir}/${sim3Dir} ${cDir}/${sim4Dir}
 
 # this makes x1 node job (running x4 simulations) runs for 1h
@@ -26,6 +29,8 @@ bsub -nnodes 1 -W 60 -G cancer -q pbatch ${cDir}/run_gmx.sh 1 ${cDir}/${sim1Dir}
 cd henm 
 # consider what input/output and location
 # GROMACS trajectory files will be in each simulation's subdirectory
+
+# Runs henm script
 bash run-henm.sh
 
 ##########################
@@ -34,4 +39,5 @@ bash run-henm.sh
 
 cd ../lammps
 
+# Runs the LAMMPS script
 bash run-lammps.sh
